30d04f269cfba3b2660a85958a2104dbd32ddd0d
hurshik7 <hurshik7@gmail.com>
2023-03-03 11:53:40 -0800

    separate header, remove main() in ndbm.c

---
+++ include/my_ndbm.h
@@ -0,0 +1,23 @@
+#ifndef NDBM_NDBM_H
+#define NDBM_NDBM_H
+
+
+#include <ndbm.h>
+
+
+#define MAX_FIRST_NAME_LENGTH 20
+#define MAX_LAST_NAME_LENGTH 30
+
+
+typedef struct User {
+    char firstName[MAX_FIRST_NAME_LENGTH];
+    char lastName[MAX_LAST_NAME_LENGTH];
+} user_t;
+
+void printUser(struct User user);
+DBM* createDatabase(void);
+void insertUser(DBM *db);
+void fetchUser(DBM *db);
+void optionHandler(DBM* db);
+
+#endif //NDBM_NDBM_H
