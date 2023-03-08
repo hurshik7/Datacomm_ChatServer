72e274e76f44fd582271623d8c1e8fc259c67e0b
hurshik7 <hurshik7@gmail.com>
2023-03-08 11:17:21 -0800

    add macros, UserLoginInfo, UserAccountInfo structs

--- include/my_ndbm.h
+++ include/my_ndbm.h
@@ -1,25 +1,48 @@
-#ifndef NDBM_NDBM_H
-#define NDBM_NDBM_H
+#ifndef MY_NDBM_H
+#define MY_NDBM_H
 
 
 #include <ndbm.h>
+#include <netinet/in.h>
+#include <stdbool.h>
+#include <uuid/uuid.h>
 
 
 #define MAX_FIRST_NAME_LENGTH 20
 #define MAX_LAST_NAME_LENGTH 30
+#define TOKEN_NAME_LENGTH (20)
+#define PSWD_MAX_LENGTH (30)
 
 
-typedef struct User {
-    char firstName[MAX_FIRST_NAME_LENGTH];
-    char lastName[MAX_LAST_NAME_LENGTH];
-} user_t;
+#define DB_LOGIN_INFO "user_login_info"
+#define DB_USER_ACCOUNT "user_account_info"
 
-void printUser(struct User user);
-DBM* createDatabase(void);
+
+typedef struct UserLoginInfo {
+    char login_token[TOKEN_NAME_LENGTH];
+    char password[PSWD_MAX_LENGTH];
+    uuid_t* id;
+} user_login_t;
+
+typedef struct UserAccountInfo {
+    uuid_t* user_id;
+    char display_name[TOKEN_NAME_LENGTH];
+    struct sockaddr_in sock_addr;
+    bool online_status;
+    int privilege_level;
+} user_account_t;
+
+
+DBM* open_db_or_null(const char *db_name);
+user_login_t* get_login_info_malloc_or_null(char *login_token);
+
+/*
 void insertUser(DBM *db);
 void fetchUser(DBM *db);
 void deleteUser(DBM *db);
 void printAllUsers(DBM *db);
 void optionHandler(DBM* db);
+*/
 
-#endif //NDBM_NDBM_H
+#endif /* MY_NDBM_H */
+
