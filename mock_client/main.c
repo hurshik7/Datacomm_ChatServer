0b6e0f9786842e0c05ac697a68168fdef27cbbc4
bennychao <bchao5@my.bcit.ca>
2023-03-11 00:40:47 -0800

    Take user input now to test CREATE_USER or CREATE_AUTH

--- mock_client/main.c
+++ mock_client/main.c
@@ -27,6 +27,7 @@ typedef struct chat_header {
 int copy(int from_fd, int to_fd, size_t count);
 
 int main(int argc, char const *argv[]) {
+
     int client_socket;
     struct sockaddr_in server;
     char received_size[128];
@@ -40,6 +41,7 @@ int main(int argc, char const *argv[]) {
     }
 
     // prepare the sockaddr_in structure
+//    server.sin_addr.s_addr = inet_addr("192.168.1.85");
     server.sin_addr.s_addr = inet_addr("127.0.0.1");
     server.sin_family = AF_INET;
     server.sin_port = htons(5050);
@@ -50,47 +52,97 @@ int main(int argc, char const *argv[]) {
         return 1;
     }
 
-    chat_header_t test_header;
-    memset(&test_header, 0, sizeof(chat_header_t));
-    test_header.version_type.version = 1;
-    test_header.version_type.type = 1;
-    test_header.object = 1;
-    // for testing read_header
-    printf("version: %d\n", test_header.version_type.version);
-    printf("type: %d\n", test_header.version_type.type);
-    printf("object: %d\n", test_header.object);
+    char choice;
 
-    char body[] = "shikhur\3shik\3hur123\0";
-    test_header.body_size = (uint16_t) strlen(body);
-    uint16_t body_size = test_header.body_size;
-    test_header.body_size = htons(test_header.body_size);
+    printf("%s", "[1] - CREATE_USER\n"
+                 "[2] - CREATE_AUTH");
+    scanf("%c", &choice);
 
-    uint32_t temp_int = 0;
-    memcpy(&temp_int, &test_header, sizeof(test_header));
+    if (choice == '1') {
+        // CREATE USER
+        chat_header_t test_header;
+        memset(&test_header, 0, sizeof(chat_header_t));
+        test_header.version_type.version = 1;
+        test_header.version_type.type = 1;
+        test_header.object = 1;
+        // for testing read_header
+        printf("version: %d\n", test_header.version_type.version);
+        printf("type: %d\n", test_header.version_type.type);
+        printf("object: %d\n", test_header.object);
+
+        char body[] = "bennychao\3benny\3monkey123\0";
+        test_header.body_size = (uint16_t) strlen(body);
+        uint16_t body_size = test_header.body_size;
+        test_header.body_size = htons(test_header.body_size);
+        uint32_t temp_int = 0;
+        memcpy(&temp_int, &test_header, sizeof(test_header));
+
+        while (1) {
+            if (send(client_socket, &temp_int, sizeof(uint32_t), 0) < 0) {
+                perror("send");
+            }
+
+            if (send(client_socket, body, body_size, 0) < 0) {
+                perror("send");
+            }
+
+            char buffer[DEFUALT_BUFFER];
+            if (read(client_socket, buffer, sizeof(test_header)) < 0) {
+                perror("read");
+            }
+            memset(buffer, '\0', DEFUALT_BUFFER);
+            if (read(client_socket, buffer, DEFUALT_BUFFER) < 0) {
+                perror("recv");
+            }
+            printf("res: %s\n", buffer);
+            sleep(5);
+        }
+    } else if (choice == '2') {
+        // CREATE_AUTH
+        chat_header_t test_header;
+        memset(&test_header, 0, sizeof(chat_header_t));
+        test_header.version_type.version = 1;
+        test_header.version_type.type = 1;
+        test_header.object = 4;
+        // for testing read_header
+        printf("version: %d\n", test_header.version_type.version);
+        printf("type: %d\n", test_header.version_type.type);
+        printf("object: %d\n", test_header.object);
+
+        char body[] = "bennychao\3monkey123\0";
+        test_header.body_size = (uint16_t) strlen(body);
+        uint16_t body_size = test_header.body_size;
+        test_header.body_size = htons(test_header.body_size);
+
+        uint32_t temp_int = 0;
+        memcpy(&temp_int, &test_header, sizeof(test_header));
+
+        while (1) {
+            if (send(client_socket, &temp_int, sizeof(uint32_t), 0) < 0) {
+                perror("send");
+            }
+
+            if (send(client_socket, body, body_size, 0) < 0) {
+                perror("send");
+            }
+
+            char buffer[DEFUALT_BUFFER];
+            if (read(client_socket, buffer, sizeof(test_header)) < 0) {
+                perror("read");
+            }
+            memset(buffer, '\0', DEFUALT_BUFFER);
+            if (read(client_socket, buffer, DEFUALT_BUFFER) < 0) {
+                perror("recv");
+            }
+            printf("res: %s\n", buffer);
+            sleep(5);
+        }
+    }
+
 //    printf("temp_int: %d\n", temp_int);
 //    temp_int = htonl(temp_int);
 //    printf("temp_int after htonl(): %d\n", temp_int);
 
-    while (1) {
-        if (send(client_socket, &temp_int, sizeof(uint32_t), 0) < 0) {
-            perror("send");
-        }
-
-        if (send(client_socket, body, body_size, 0) < 0) {
-            perror("send");
-        }
-
-        char buffer[DEFUALT_BUFFER];
-        if (read(client_socket, buffer, sizeof(test_header)) < 0) {
-            perror("read");
-        }
-        memset(buffer, '\0', DEFUALT_BUFFER);
-        if (read(client_socket, buffer, DEFUALT_BUFFER) < 0) {
-            perror("recv");
-        }
-        printf("res: %s\n", buffer);
-        sleep(5);
-    }
     // close the file and the socket.
     //close(client_socket);
     return 0;
