3def1ff2729fc2605973bab4b10090696acbde32
Benny Chao <bchao5@my.bcit.ca>
2023-03-13 01:30:18 -0700

    Added function in db_viewer to detect and switch db directories depending on os

--- db_viewer/main.c
+++ db_viewer/main.c
@@ -7,6 +7,7 @@
 #include <arpa/inet.h>
 // fcntl must be explicitly declared when running db_viewer on Linux
 #include <fcntl.h>
+#include <sys/utsname.h>
 
 
 #define TOKEN_NAME_LENGTH (20)
@@ -24,9 +25,9 @@
 /**
  * db files stored here for Linux distros
  */
-//#define DB_LOGIN_INFO "../src/user_login_info"
-//#define DB_DISPLAY_NAMES "../src/display_names"
-//#define DB_USER_ACCOUNT "../src/user_account_info"
+#define DB_LOGIN_INFO_LINUX "../src/user_login_info"
+#define DB_DISPLAY_NAMES_LINUX "../src/display_names"
+#define DB_USER_ACCOUNT_LINUX "../src/user_account_info"
 
 
 typedef struct UserLoginInfo {
@@ -47,21 +48,36 @@ void printAllUserAccounts(DBM *db);
 void printAllDisplayNames(DBM *db);
 void printAllLoginInfos(DBM *db);
 void get_ip_str(const struct sockaddr *sa, char *out_str);
+struct utsname identify_os(void);
 
 int main(int argc, char* argv[])
 {
+    char *user_acc = malloc(sizeof(char) * 256);
+    char *user_dsply_nm = malloc(sizeof(char) * 256);
+    char *user_log_info = malloc(sizeof(char) * 256);
+    struct utsname my_data = identify_os();
+    char* my_os = my_data.sysname;
+    if (strcmp(my_os, "Darwin") == 0) {
+        strncpy(user_acc, DB_USER_ACCOUNT, strlen(DB_USER_ACCOUNT));
+        strncpy(user_dsply_nm, DB_DISPLAY_NAMES, sizeof(DB_DISPLAY_NAMES));
+        strncpy(user_log_info, DB_LOGIN_INFO, sizeof(DB_LOGIN_INFO));
+    } else if (strcmp(my_os, "Linux") == 0) {
+        strncpy(user_acc, DB_USER_ACCOUNT_LINUX, sizeof(DB_USER_ACCOUNT_LINUX));
+        strncpy(user_dsply_nm, DB_DISPLAY_NAMES_LINUX, sizeof(DB_DISPLAY_NAMES_LINUX));
+        strncpy(user_log_info, DB_LOGIN_INFO_LINUX, sizeof(DB_LOGIN_INFO_LINUX));
+    }
     puts("UserAccounts DB");
-    DBM* user_accounts = dbm_open(DB_USER_ACCOUNT, O_RDWR | O_SYNC | O_APPEND, 0644);
+    DBM* user_accounts = dbm_open(user_acc, O_RDWR | O_SYNC | O_APPEND, 0644);
     printAllUserAccounts(user_accounts);
     puts("");
 
     puts("DisplayNames DB");
-    DBM* display_names = dbm_open(DB_DISPLAY_NAMES, O_RDWR | O_SYNC | O_APPEND, 0644);
+    DBM* display_names = dbm_open(user_dsply_nm, O_RDWR | O_SYNC | O_APPEND, 0644);
     printAllDisplayNames(display_names);
     puts("");
 
     puts("UserLoginInfo DB");
-    DBM* login_infos = dbm_open(DB_LOGIN_INFO, O_RDWR | O_SYNC | O_APPEND, 0644);
+    DBM* login_infos = dbm_open(user_log_info, O_RDWR | O_SYNC | O_APPEND, 0644);
     printAllLoginInfos(login_infos);
     puts("");
 
@@ -129,5 +145,18 @@ void get_ip_str(const struct sockaddr *sa, char *out_str)
             strncpy(out_str, "Unknown AF", MAX_IP_ADD_STR_LENGTH);
             out_str[MAX_IP_ADD_STR_LENGTH - 1] = '\0';
     }
+}
+
+struct utsname identify_os(void) {
+    struct utsname unameData;
+    int result = uname(&unameData);
+
+    if (result == 0) {
+        printf("Server is running on: %s\n", unameData.sysname);
+    } else {
+        printf("Error getting uname information\n");
+    }
+
+    return unameData;
 }
 
