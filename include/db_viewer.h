cb2a427e862cec0993548e4de1d8c7f9a2cdafd5
hurshik7 <hurshik7@gmail.com>
2023-03-22 16:58:40 -0700

    add db_viewer

---
+++ include/db_viewer.h
@@ -0,0 +1,39 @@
+#ifndef CHAT_SERVER_DB_VIEWER_H
+#define CHAT_SERVER_DB_VIEWER_H
+
+
+#include "my_ndbm.h"
+#include <ndbm.h>
+#include <netinet/in.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <arpa/inet.h>
+// fcntl must be explicitly declared when running db_viewer on Linux
+#include <fcntl.h>
+#include <sys/utsname.h>
+
+/**
+ * db files stored here for macOS
+ */
+#define DB_VIEWER_LOGIN_INFO_LOC "../cmake-build-debug/user_login_info"
+#define DB_VIEWER_DISPLAY_NAMES_LOC "../cmake-build-debug/display_names"
+#define DB_VIEWER_USER_ACCOUNT_LOC "../cmake-build-debug/user_account_info"
+
+/**
+ * db files stored here for Linux distros
+ */
+#define DB_LOGIN_INFO_LINUX "../src/user_login_info"
+#define DB_DISPLAY_NAMES_LINUX "../src/display_names"
+#define DB_USER_ACCOUNT_LINUX "../src/user_account_info"
+
+
+void printAllUserAccounts(DBM *db);
+void printAllDisplayNames(DBM *db);
+void printAllLoginInfos(DBM *db);
+void run_db_viewer(void);
+
+
+#endif //CHAT_SERVER_DB_VIEWER_H
+
