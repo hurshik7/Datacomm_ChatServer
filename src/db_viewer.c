c6ee6b1327bedfecb716a7923776d152a78971ea
hurshik7 <hurshik7@gmail.com>
2023-04-04 19:59:15 -0700

    remove comments

--- src/db_viewer.c
+++ src/db_viewer.c
@@ -83,21 +83,6 @@ void printAllChannelInfos(DBM *db)
 
 void run_db_viewer(void)
 {
-    // TODO Erase the comments after testing (SHIK, change db paths)
-//    char *user_acc = malloc(sizeof(char) * 256);
-//    char *user_dsply_nm = malloc(sizeof(char) * 256);
-//    char *user_log_info = malloc(sizeof(char) * 256);
-//    struct utsname my_data = identify_os();
-//    char* my_os = my_data.sysname;
-//    if (strcmp(my_os, "Darwin") == 0) {
-//        strncpy(user_acc, DB_VIEWER_USER_ACCOUNT_LOC, strlen(DB_VIEWER_USER_ACCOUNT_LOC));
-//        strncpy(user_dsply_nm, DB_VIEWER_DISPLAY_NAMES_LOC, sizeof(DB_VIEWER_DISPLAY_NAMES_LOC));
-//        strncpy(user_log_info, DB_VIEWER_LOGIN_INFO_LOC, sizeof(DB_VIEWER_LOGIN_INFO_LOC));
-//    } else if (strcmp(my_os, "Linux") == 0) {
-//        strncpy(user_acc, DB_USER_ACCOUNT_LINUX, sizeof(DB_USER_ACCOUNT_LINUX));
-//        strncpy(user_dsply_nm, DB_DISPLAY_NAMES_LINUX, sizeof(DB_DISPLAY_NAMES_LINUX));
-//        strncpy(user_log_info, DB_LOGIN_INFO_LINUX, sizeof(DB_LOGIN_INFO_LINUX));
-//    }
     printw("UserAccounts DB\n");
     DBM* user_accounts = dbm_open(DB_USER_ACCOUNT_PATH, O_RDWR | O_SYNC | O_APPEND, 0644);
     if (user_accounts != NULL) {
@@ -128,9 +113,6 @@ void run_db_viewer(void)
     refresh();
 
     // deallocate memory
-//    free(user_acc);
-//    free(user_dsply_nm);
-//    free(user_log_info);
     dbm_close(login_infos);
     dbm_close(display_names);
     dbm_close(user_accounts);
