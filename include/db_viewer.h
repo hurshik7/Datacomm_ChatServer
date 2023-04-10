c40dee3f015f18b15727be152f1e7a15f056f23e
hurshik7 <hurshik7@gmail.com>
2023-04-10 15:27:43 -0700

    remove warnings

--- include/db_viewer.h
+++ include/db_viewer.h
@@ -10,16 +10,8 @@
 #include <stdlib.h>
 #include <string.h>
 #include <arpa/inet.h>
-// fcntl must be explicitly declared when running db_viewer on Linux
 #include <fcntl.h>
 #include <sys/utsname.h>
-
-/**
- * db files stored here for macOS
- */
-#define DB_VIEWER_LOGIN_INFO_LOC "../cmake-build-debug/user_login_info"
-#define DB_VIEWER_DISPLAY_NAMES_LOC "../cmake-build-debug/display_names"
-#define DB_VIEWER_USER_ACCOUNT_LOC "../cmake-build-debug/user_account_info"
 
 
 void printAllUserAccounts(DBM *db);
