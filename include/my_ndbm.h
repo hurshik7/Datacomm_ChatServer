2b234d79acdbd4bc5f1a1df2db15782f95173ad7
hurshik7 <hurshik7@gmail.com>
2023-04-16 15:14:53 -0700

    change the type of time_stamp

--- include/my_ndbm.h
+++ include/my_ndbm.h
@@ -56,7 +56,7 @@ typedef struct MessageInfo {
     char user_id[UUID_LEN];
     char channel_id[UUID_LEN];
     char message_content[MAX_MESSAGE_SIZE];
-    uint8_t time_stamp[TIMESTAMP_SIZE];
+    char time_stamp[TIMESTAMP_SIZE];
 } message_info_t;
 
 typedef struct {
